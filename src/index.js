358f52949f7c957e2a924216c4d752998ba6b53d
Sina Aghli <sina@Sinas-MacBook-Pro.local>
2024-10-22 14:51:37 -0600

    update logo

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 import './style.css';
-import constructionImagePath from './construction.svg'
+import constructionImagePath from './LOGO.png'
 document.querySelector("img").src = constructionImagePath;
